b277e62d32eab9b8820d94a4cf1f52675e199615
Christia Troyer <christia.troyer@civicactions.com>
2023-09-06 07:40:29 -0700

    Add updb

--- refresh.sh
+++ refresh.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Name of upstream remote
+UPSTREAM=origin
+
+# Name of main branch
+MAIN_BRANCH=main
+
 echo "Fetching upstream stuff..."
 git fetch --all
-echo "Rebasing with upstream/main..."
-git rebase upstream/main
+echo "Rebasing with the main branch..."
+git rebase $UPSTREAM/$MAIN_BRANCH
 echo "Running composer install..."
 ddev composer install
 echo "Running database updates..."
 ddev drush updb -y
+echo "Importing config"
+ddev drush cim -y
