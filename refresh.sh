5763419d1f334d9ace3a5ab08829362404f97f54
Christia Troyer <christia.troyer@civicactions.com>
2023-09-12 23:04:57 -0700

    Changing order of opperations

--- refresh.sh
+++ refresh.sh
@@ -12,7 +12,8 @@ echo "Rebasing with the main branch..."
 git rebase $UPSTREAM/$MAIN_BRANCH
 echo "Running composer install..."
 ddev composer install
-echo "Running database updates..."
-ddev drush updb -y
 echo "Importing config"
 ddev drush cim -y
+echo "Database updates..."
+ddev drush updb -y
+ddev composer run va:theme:compile
