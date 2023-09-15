1df4d97b11c31494d7883cdec8c9bc670ebdf254
Christia Troyer <christia.troyer@civicactions.com>
2023-09-15 02:27:23 -0700

    Total rewrite

--- refresh.sh
+++ refresh.sh
@@ -1,27 +1,68 @@
 #!/bin/bash
 
-# Name of upstream remote
-UPSTREAM=origin
+# Exit script if any command fails
+set -e
+
+# Check these variables
+# Name of remotes
+UPSTREAM=vagovcms
+MYFORK=chri5tia
 
 # Name of main branch
 MAIN_BRANCH=main
 
 # Decoration
-D=☆⋆｡𖦹°‧★☆⋆｡𖦹°‧★☆⋆｡𖦹°‧★
+DECO=⋆⊹₊☆⋆｡°‧★☆⋆‧⋆⊹₊☆⋆｡‧★☆⋆‧
+#DECO= -------------
 
-echo "$D Fetching upstream stuff. $D"
+# Decoration function
+echo_deco() {
+  local prefix="$DECO"
+  local suffix="$DECO"
+  echo "${prefix} ${1} ${suffix}"
+}
+
+echo_deco "Fetching upstream stuff."
 git fetch --all
-echo "$D Rebasing with the main branch. $D"
+
+echo_deco "Rebasing with the main branch."
 git rebase $UPSTREAM/$MAIN_BRANCH
-echo "$D Running composer install. $D"
+
+echo_deco "Running composer install."
 ddev composer install
-echo "$D Uninstalling devel $D"
-ddev drush pm-uninstall devel
-echo "$D Importing config. $D"
+
+devel_status=$(ddev drush pml | grep devel)
+if [[ $devel_status == *enabled ]]; then
+  echo_deco "Uninstalling devel"
+  ddev drush pm-uninstall devel
+else
+  echo_deco "Devel is already not enabled. Moving on."
+fi
+
+echo_deco "Importing config."
 ddev drush cim -y
-echo "$D Database updates. $D"
+
+echo_deco "Database updates."
 ddev drush updb -y
-echo "$D Compiling the theme $D"
-ddev composer run va:theme:compile
-echo "$D Reinstalling devel $D"
+
+read -p "Do I need to compile the theme?" theme
+if [[ $theme == "y" || $theme == "Y" ]]; then
+  echo_deco "Fine. Compiling the theme. This will take awhile."
+  ddev composer run va:theme:compile
+fi
+
+echo_deco "Clearing Drupal caches."
+ddev drush cr
+
+echo_deco "Reinstalling devel."
 ddev drush en devel
+
+echo_deco "Opening local project in browser."
+ddev drush launch
+
+read -p "Do you want to do some additional clean up?" clean
+if [[ $clean == 'y' || $clean == "Y" ]]; then
+  echo_deco "Great. Doing some maintenance. Baiii."
+  npx update-browserslist-db@latest
+
+echo_deco "Done."
