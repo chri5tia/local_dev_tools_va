5d4c27f326096cc86fb7474750977827020386b7
Christia Troyer <christia.troyer@civicactions.com>
2023-10-12 21:41:44 -0700

    Make gitco more smart

--- gitco.sh
+++ gitco.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # Handy git checkout shortcut
-read -p "Ticket ID? " ticket
-target=$(git branch | grep "$ticket")
-git checkout $target
+
+if [ -n "$1" ]; then
+    ticket="$1";
+    target=$(git branch | grep "$ticket")
+    git checkout $target
+else
+    read -p "Supply a ticket ID so I can checkout the branch for it. " ticket
+    target=$(git branch | grep "$ticket")
+    git checkout $target
+fi
